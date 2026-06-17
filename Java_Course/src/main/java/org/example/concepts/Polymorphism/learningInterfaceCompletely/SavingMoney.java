d92515197546d0b4ea285eaeb6b4f5f60d5de8e9
Surmani Konsam <109019866+SurmaniKonsam@users.noreply.github.com>
2026-06-18 00:02:37 +0530

    Basic implementation

---
+++ Java_Course/src/main/java/org/example/concepts/Polymorphism/learningInterfaceCompletely/SavingMoney.java
@@ -0,0 +1,8 @@
+package org.example.concepts.Polymorphism.learningInterfaceCompletely;
+
+public class SavingMoney implements Account{
+    @Override
+    public int getMoney() {
+        return 134233;
+    }
+}
