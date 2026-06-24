b8ce74e291587343552295b7eb5115f15da788f7
Surmani Konsam <109019866+SurmaniKonsam@users.noreply.github.com>
2026-06-24 22:24:54 +0530

    committed linear search

---
+++ Java_Course/src/main/java/org/example/dailyDSAExercise/dsa/linearSearch/Hello.java
@@ -0,0 +1,8 @@
+package org.example.dailyDSAExercise.dsa.linearSearch;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello  linear search");
+    }
+
+}
