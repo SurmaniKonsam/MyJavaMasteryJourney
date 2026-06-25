a16cf78fcf6094dd04091ca6d17073e113427090
Surmani Konsam <109019866+SurmaniKonsam@users.noreply.github.com>
2026-06-25 13:29:12 +0530

    second commit

--- Java_Course/src/main/java/org/example/dailyDSAExercise/dsa/linearSearch/LinearSearchExercise.java
+++ Java_Course/src/main/java/org/example/dailyDSAExercise/dsa/linearSearch/LinearSearchExercise.java
@@ -2,7 +2,7 @@ package org.example.dailyDSAExercise.dsa.linearSearch;
 
 
 public class LinearSearchExercise {
-    //small change to understand conflict merge
+    //small change to understand conflict merge 2 23 423 43 24 234 
     static void findMinimumElement(int ...arr){
         //[7, 2, 9, 1, 5]
         int minimumValue = Integer.MAX_VALUE;
